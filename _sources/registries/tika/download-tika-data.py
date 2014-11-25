4c2b9350777718054841476aa1d55db314124791
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-11-25 22:00:16 +0000

    Updating Tika link.

--- _sources/registries/tika/download-tika-data.py
+++ _sources/registries/tika/download-tika-data.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 import urllib
 
 # Download Tika XML sigs:
-url = "https://github.com/apache/tika/raw/trunk/tika-core/src/main/resources/org/apache/tika/mime/tika-mimetypes.xml"
+#url = "https://github.com/apache/tika/raw/trunk/tika-core/src/main/resources/org/apache/tika/mime/tika-mimetypes.xml"
+url = "https://svn.apache.org/repos/asf/tika/trunk/tika-core/src/main/resources/org/apache/tika/mime/tika-mimetypes.xml"
 urllib.urlretrieve(url,"tika-mimetypes.xml")
 
 
