ecd7d365580171bdca890a71e8df7f971344b278
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-04-27 23:08:43 +0100

    Took the UA out again as the download no longer requires it.

--- _sources/registries/fdd/download-fdd-data.py
+++ _sources/registries/fdd/download-fdd-data.py
@@ -5,7 +5,6 @@ import os
 
 # Download TRiD XML sigs:
 url = "https://www.loc.gov/preservation/digital/formats/fddXML.zip"
-urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 urllib.urlretrieve(url,"fddXML.zip")
 print("Downloaded %s" % url )
 print(os.system("ls -l fddXML.zip"))
