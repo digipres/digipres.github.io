2772c87cc547d1ed0c125f2f80ed67ed90a2c0ce
Andrew Jackson <andrew.jackson@dpconline.org>
2024-07-11 09:27:24 +0100

    Better formatting.

--- _sources/registries/wikidata/download-wikidata-formats.py
+++ _sources/registries/wikidata/download-wikidata-formats.py
@@ -8,28 +8,36 @@ url = 'https://query.wikidata.org/sparql'
 query = '''
 # Return all file format and family records from Wikidata.
 #
-select distinct ?uri ?uriLabel ?puid ?extension ?mimetype ?encodingLabel ?referenceLabel ?date ?relativityLabel ?offset ?sig
-where
+SELECT DISTINCT ?uri ?uriLabel ?puid ?extension ?mimetype ?ffw ?lcfdd ?narafpid ?encodingLabel ?referenceLabel ?date ?relativityLabel ?offset ?sig
+WHERE
 {
+  # Return records of type File Format or File Format Family (via instance or subclass chain):
   { ?uri wdt:P31/wdt:P279* wd:Q235557 } UNION 
-    { ?uri wdt:P31/wdt:P279* wd:Q26085352 }.       # Return records of type File Format and File Format Family.
-  optional { ?uri wdt:P2748 ?puid.      }          # PUID is used to map to PRONOM signatures proper.
-  optional { ?uri wdt:P1195 ?extension. }
-  optional { ?uri wdt:P1163 ?mimetype.  }
-  optional { ?uri p:P4152 ?object;                 # Format identification pattern statement.
-    optional { ?object pq:P3294 ?encoding.   }     # We don't always have an encoding.
-    optional { ?object ps:P4152 ?sig.        }     # We always have a signature.
-    optional { ?object pq:P2210 ?relativity. }     # Relativity to beginning or end of file.
-    optional { ?object pq:P4153 ?offset.     }     # Offset relatve to the relativity.
-    optional { ?object prov:wasDerivedFrom ?provenance;
-       optional { ?provenance pr:P248 ?reference;
+    { ?uri wdt:P31/wdt:P279* wd:Q26085352 }.
+      
+  # Only return records that have at least one useful format identifier
+  FILTER EXISTS { ?uri wdt:P2748|wdt:P1195|wdt:P1163|wdt:P3381|wdt:P3266|wdt:P11167 [] }.       
+  
+  OPTIONAL { ?uri wdt:P2748 ?puid.      }          # PUID is used to map to PRONOM signatures
+  OPTIONAL { ?uri wdt:P1195 ?extension. }          # File extension
+  OPTIONAL { ?uri wdt:P1163 ?mimetype.  }          # IANA Media Type
+  OPTIONAL { ?uri wdt:P3381 ?ffw. }                # Entry in the Just Solve File Formats Wiki
+  OPTIONAL { ?uri wdt:P3266 ?lcfdd. }              # Library of Congress Format Descriptions ID
+  OPTIONAL { ?uri wdt:P11167 ?narafpid. }          # NARA File Format Preservation Plan ID
+  OPTIONAL { ?uri p:P4152 ?object;                 # Format identification pattern statement
+    OPTIONAL { ?object pq:P3294 ?encoding.   }     # We don't always have an encoding
+    OPTIONAL { ?object ps:P4152 ?sig.        }     # We always have a signature
+    OPTIONAL { ?object pq:P2210 ?relativity. }     # Relativity to beginning or end of file
+    OPTIONAL { ?object pq:P4153 ?offset.     }     # Offset relative to the relativity
+    OPTIONAL { ?object prov:wasDerivedFrom ?provenance;
+       OPTIONAL { ?provenance pr:P248 ?reference;
                               pr:P813 ?date.
                 }
     }
   }
-  service wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], en". }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], en". }
 }
-order by ?uri
+ORDER BY ?uri
 '''
 
 r = requests.get(url, params = {'format': 'json', 'query': query})
@@ -38,6 +46,7 @@ data = r.json()
 format_data = []
 
 for r in data['results']['bindings']:
+    # Filter down to items with at least one of the identifiers we care about:
     if any(x in r for x in ['puid','extension','mimetype']):
         # Print a sample
         if len(format_data) == 0:
@@ -49,6 +58,7 @@ for r in data['results']['bindings']:
             'source': r['uri']['value'],
             'name': r['uriLabel']['value'],
         }
+        # Note that the mutiple fields, e.g. extensions, end up output in diffferent orders each time, which means that diffs are difficult to understand:
         for x in ['puid','extension','mimetype', 'offset', 'sig']:
             if x in r:
                 item[x] = r[x]['value']
