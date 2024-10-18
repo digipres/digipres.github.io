29a3467fb5c1e9d0aeac47e9f0ea93c4799fa488
Andrew Jackson <andrew.jackson@dpconline.org>
2024-10-18 14:50:47 +0100

    Removed content that will live elsewhere.

--- _sources/registries/githublinguist/download-githublinguist-data.py
+++
@@ -1,7 +0,0 @@
-from __future__ import with_statement
-from __future__ import print_function
-from urllib.request import urlretrieve
-
-# Download Github Linguist language sigs:
-url = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
-urlretrieve(url,"languages.yml")
